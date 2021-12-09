be4c716b2f8d58bbdb26305aa607c8533a0e6e8a
katerinatrajcevska <katerinatrajchevska23@gmail.com>
2021-12-09 14:54:31 +0100

    Commit

---
+++ src/Zadacha1/GlavnaPrograma.java
@@ -0,0 +1,16 @@
+package Zadacha1;
+import java.util.ArrayList;
+import java.util.List;
+public class GlavnaPrograma {
+
+    public static void main(String[] args) {
+        List<String> lista = new ArrayList<String>();
+        lista.add("Bitola");
+        lista.add("Skopje");
+        lista.add("Gostivar");
+        lista.add("Krusevo");
+        lista.add("Ohrid");
+
+        System.out.println(lista);
+    }
+}
---
+++ src/Zadacha8/GlavnaPrograma.java
@@ -0,0 +1,15 @@
+package Zadacha8;
+import java.util.ArrayList;
+import java.util.List;
+public class GlavnaPrograma {
+    public static void main(String[] args) {
+        List<String> lista = new ArrayList<String>();
+        for(int i=5;i<=20;i+=5){
+            lista.add("mail"+i+"@yahoo.com");
+        }
+
+        for(int i=0;i<lista.size();i++){
+            System.out.println(lista.get(i));
+        }
+    }
+}
