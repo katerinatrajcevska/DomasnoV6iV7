66c6a8ebe05e506d9e9c62308f8dae0b580a40a9
katerinatrajcevska <katerinatrajchevska23@gmail.com>
2021-12-09 15:17:09 +0100

    Commit

---
+++ src/Zadacha4/GlavnaPrograma.java
@@ -0,0 +1,17 @@
+package Zadacha4;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class GlavnaPrograma {
+    public static void main(String[] args) {
+        Set<String> mnozestvo = new HashSet<String>();
+        mnozestvo.add("Ruzha");
+        mnozestvo.add("Kokiche");
+        mnozestvo.add("Jorgovan");
+        mnozestvo.add("Lavanda");
+        mnozestvo.add("Kaktus");
+
+        System.out.println(mnozestvo);
+    }
+}
